a7f1394ca3970318b78a1726cae6f56ae90e81b6
Ivan <thegarbagepersonman@gmail.com>
2024-05-15 22:02:30 -0400

    fixed size level determination, less this time

--- minesweeper.js
+++ minesweeper.js
@@ -266,8 +266,8 @@ function calc_difficulty(game)
 	const mine_density = game.mines / game_cells;
 	level.mine_level = 1 + Math.ceil((mine_density - min_mine_density) / mine_density_multiplier);
 
-	const average_dimension = (game.height + game.width) / 2;
-	level.size_level = (average_dimension - size_level_multiplier) / size_level_multiplier;
+	const average_dimension = (game.height + game.width) / 2;  // TODO: implement this instead of height for size_level calculation
+	level.size_level = (game.height - size_level_multiplier) / size_level_multiplier;
 
 	return level;
 }
