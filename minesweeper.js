7e5c020ca930f179be4b5657db88a07684edee29
tartarus6 <thegarbagepersonman@gmail.com>
2024-05-16 12:16:27 -0400

    bad

--- minesweeper.js
+++ minesweeper.js
@@ -224,7 +224,7 @@ function isgamewon()
 				best = {};
 			// delete best[level.size_level][level.mine_level];
 			console.pause();  // 2
-			best[ceil_level["size_level"]][ceil_level["mine_level"]] = "game";
+			best[0][0] = "game";
 		}
 		console.pause();  // 3
 		game.name = user.alias;
