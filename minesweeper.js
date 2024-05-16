202b8ad9d4beb7e4e133d296930f3b0e05f0023e
Ivan <thegarbagepersonman@gmail.com>
2024-05-15 22:05:08 -0400

    fixed using area instead of height for size level

--- minesweeper.js
+++ minesweeper.js
@@ -267,7 +267,7 @@ function calc_difficulty(game)
 	level.mine_level = 1 + Math.ceil((mine_density - min_mine_density) / mine_density_multiplier);
 
 	const game_cells = game.height * game.width;
-	level.size_level = (game_cells - size_level_multiplier) / size_level_multiplier;
+	level.size_level = (game.height - size_level_multiplier) / size_level_multiplier;
 
 	return level;
 }
