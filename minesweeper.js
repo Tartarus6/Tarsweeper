71dfa69fbdcb5ca8534348478c410152c03386f6
Ivan <thegarbagepersonman@gmail.com>
2024-05-15 22:19:29 -0400

    moved level down a line on main screen

--- minesweeper.js
+++ minesweeper.js
@@ -744,12 +744,16 @@ function draw_board(full)
 		}
 		var timeleft = Math.max(0, (options.timelimit * 60) - elapsed);
 		console.attributes = LIGHTCYAN;
-		console_center(format("%2d Mines  Lvl %1.2f-%1.2f  %s%s "
-			, game.mines - totalflags(), calc_difficulty(game).size_level
-			, calc_difficulty(game).mine_level
-			, game.start && !gameover && (timeleft / 60) <= options.timewarn ? "\x01r\x01h\x01i" : ""
-			, secondstr(elapsed)
+		console_center(format("%2d Mines  %s%s ",
+			game.mines - totalflags(),
+			game.start && !gameover && (timeleft / 60) <= options.timewarn ? "\x01r\x01h\x01i" : "",
+			secondstr(elapsed)
 			));
+		
+		console_center(format("Lvl %1.2f-%1.2f"),
+			calc_difficulty(game).size_level,
+			calc_difficulty(game).mine_level
+		)
 	}
 	var cmds = "";
 	if(full || cmds !== cmds_shown) {
