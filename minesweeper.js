5196a89f332f8b2eef6505172fabd7a91b502dbd
Tartarus6 <ivanglott@gmail.com>
2024-06-03 21:27:31 -0400

    debug

--- minesweeper.js
+++ minesweeper.js
@@ -1046,6 +1046,7 @@ function get_difficulty(all)
 		else if(key == 'Q') {  // let user quit out of level selector
 			result.size_level = -1;
 			console.print("quit");
+			console.pause();
 		}
 		result.size_level = key;
 
@@ -1059,6 +1060,7 @@ function get_difficulty(all)
 		else if(key == 'Q') {  // let user quit out of level selector
 			result.size_level = -1;
 			console.print("quit");
+			console.pause();
 		}
 		result.mine_level = key;
 
