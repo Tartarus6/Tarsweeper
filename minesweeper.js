c8fa98e573eebcab91d02233572088a268786966
Tartarus6 <ivanglott@gmail.com>
2024-06-06 13:30:34 -0400

    changed console.center to console_center in level select

--- minesweeper.js
+++ minesweeper.js
@@ -1069,7 +1069,7 @@ function get_difficulty(all)
 	if(all) {
 		// ask user for size level
 		console.clearline();
-		console.center(format("\x01hSize Level (%s) [\x01~All]: ", size_lvls));
+		console_center(format("\x01hSize Level (%s) [\x01~All]: ", size_lvls));
 		var key = console.getkeys("QA", max_size_level);
 		if(key == 'A') {
 			result.size_level = 0;
@@ -1084,7 +1084,7 @@ function get_difficulty(all)
 		// ask user for mine level
 		console.up();  // kind of a hack. for some reason there was a gap between the two prompts
 		console.clearline();
-		console.center(format("\x01hMine Level (%s) [\x01~All]: ", mine_lvls));
+		console_center(format("\x01hMine Level (%s) [\x01~All]: ", mine_lvls));
 		key = console.getkeys("QA", max_mine_level);
 		if(key == 'A') {
 			result.mine_level = 0;
@@ -1100,7 +1100,7 @@ function get_difficulty(all)
 	}
 
 	console.clearline();
-	console.center(format("\x01hSize Level (%s): ", size_lvls));
+	console_center(format("\x01hSize Level (%s): ", size_lvls));
 	// result.size_level = console.getnum(max_size_level);
 	key = console.getkeys("Q", max_size_level);
 	if(key == 'Q') {  // let user quit out of level selector
@@ -1111,7 +1111,7 @@ function get_difficulty(all)
 	
 	console.up();  // kind of a hack. for some reason there was a gap between the two prompts
 	console.clearline();
-	console.center(format("\x01hMine Level (%s): ", mine_lvls));
+	console_center(format("\x01hMine Level (%s): ", mine_lvls));
 	// result.mine_level = console.getnum(max_mine_level);
 	key = console.getkeys("Q", max_mine_level);
 	if(key == 'Q') {  // let user quit out of level selector
